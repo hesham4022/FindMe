db8bf92a4bdd35ac5b11b8fb073189f5d8d886e7
Hesham Hamd <heshamrdwan2004@gmail.com>
2026-03-23 17:58:10 +0200

    auto-commit

--- lib/features/Home/presentation/cubit/recent_cases_cubit/recent_cases_cubit.dart
+++ lib/features/Home/presentation/cubit/recent_cases_cubit/recent_cases_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:find_me_app/core/error_management/failure.dart';
+import 'package:find_me_app/features/all_cases/data/model/case_model_info.dart';
 
 part 'recent_cases_state.dart';
 
--- lib/features/Home/presentation/cubit/recent_cases_cubit/recent_cases_state.dart
+++ lib/features/Home/presentation/cubit/recent_cases_cubit/recent_cases_state.dart
@@ -1,10 +1,51 @@
 part of 'recent_cases_cubit.dart';
 
-sealed class RecentCasesState extends Equatable {
-  const RecentCasesState();
+enum RecentCasesStatus { initial, loading, success, error }
+
+class RecentCasesState extends Equatable {
+  final List<CaseInfoModel> recentCases;
+  final RecentCasesStatus status;
+  final Failure? failure;
+  final bool isScroll;
+
+  bool get isInitial => status == RecentCasesStatus.initial;
+  bool get isLoading => status == RecentCasesStatus.loading;
+  bool get isSuccess => status == RecentCasesStatus.success;
+  bool get isError => status == RecentCasesStatus.error;
+
+  const RecentCasesState({
+    required this.recentCases,
+    required this.status,
+    this.failure,
+    this.isScroll = false,
+  });
+
+  factory RecentCasesState.initial() {
+    return const RecentCasesState(
+      recentCases: [],
+      status: RecentCasesStatus.initial,
+    );
+  }
+
+  RecentCasesState copyWith({
+    List<CaseInfoModel>? recentCases,
+    RecentCasesStatus? status,
+    Failure? failure,
+    bool? isScroll,
+  }) {
+    return RecentCasesState(
+      recentCases: recentCases ?? this.recentCases,
+      status: status ?? this.status,
+      failure: failure ?? this.failure,
+      isScroll: isScroll ?? this.isScroll,
+    );
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [
+        recentCases,
+        status,
+        failure,
+        isScroll,
+      ];
 }
-
-final class RecentCasesInitial extends RecentCasesState {}
