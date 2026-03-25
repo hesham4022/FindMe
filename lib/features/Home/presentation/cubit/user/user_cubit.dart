176adc3d15cba73df8b475d8c0afc81e224325bf
Hesham Hamd <heshamrdwan2004@gmail.com>
2026-03-25 02:42:23 +0200

    auto-commit

--- lib/features/Home/presentation/cubit/user/user_cubit.dart
+++ lib/features/Home/presentation/cubit/user/user_cubit.dart
@@ -1,46 +1,46 @@
-import 'package:find_me_app/core/shared/models/userdata.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:find_me_app/core/shared/models/userdata.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 
-class UserState {
-  final UserModel? user;
+// class UserState {
+//   final UserModel? user;
 
-  UserState({this.user});
+//   UserState({this.user});
 
-  UserState copyWith({
-    UserModel? user,
-  }) {
-    return UserState(
-      user: user ?? this.user,
-    );
-  }
-}
+//   UserState copyWith({
+//     UserModel? user,
+//   }) {
+//     return UserState(
+//       user: user ?? this.user,
+//     );
+//   }
+// }
 
-class UserCubit extends Cubit<UserState> {
-  UserCubit() : super(UserState());
+// class UserCubit extends Cubit<UserState> {
+//   UserCubit() : super(UserState());
 
-  void setUser(UserModel user) {
-    emit(UserState(user: user));
-  }
+//   void setUser(UserModel user) {
+//     emit(UserState(user: user));
+//   }
 
-  void updateProfileImageLocal(String localPath) {
-    if (state.user != null) {
-      final updatedUser = state.user!.copyWith(localImage: localPath);
-      emit(state.copyWith(user: updatedUser));
+//   void updateProfileImageLocal(String localPath) {
+//     if (state.user != null) {
+//       final updatedUser = state.user!.copyWith(localImage: localPath);
+//       emit(state.copyWith(user: updatedUser));
 
-      // simulate upload
-      uploadProfileImage(localPath);
-    }
-  }
+//       // simulate upload
+//       uploadProfileImage(localPath);
+//     }
+//   }
 
-  Future<void> uploadProfileImage(String localPath) async {
-    await Future.delayed(const Duration(seconds: 2));
+//   Future<void> uploadProfileImage(String localPath) async {
+//     await Future.delayed(const Duration(seconds: 2));
 
-    const fakeServerUrl = "'https://i.pravatar.cc/120?img=12'";
-    final updatedUser = state.user!.copyWith(
-      imageUrl: fakeServerUrl,
-      localImage: null,
-    );
+//     const fakeServerUrl = "'https://i.pravatar.cc/120?img=12'";
+//     final updatedUser = state.user!.copyWith(
+//       imageUrl: fakeServerUrl,
+//       localImage: null,
+//     );
 
-    emit(state.copyWith(user: updatedUser));
-  }
-}
+//     emit(state.copyWith(user: updatedUser));
+//   }
+// }
