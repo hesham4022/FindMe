f96064f2e87a5ad0c3b666c27a0982174a64893f
Hesham Hamd <heshamrdwan2004@gmail.com>
2026-04-06 02:30:59 +0200

    auto-commit

--- lib/features/all_cases/presentation/widgets/case_card.dart
+++ lib/features/all_cases/presentation/widgets/case_card.dart
@@ -23,6 +23,245 @@ class CaseCard extends StatelessWidget {
   final VoidCallback onTap;
   get http => null;
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: onTap,
+//       child: Container(
+//         padding: EdgeInsets.all(11.w),
+//         decoration: BoxDecoration(
+//           color: const Color(0xFFE6E6FA),
+//           borderRadius: BorderRadius.circular(16.r),
+//         ),
+//         child: Row(
+//           children: [
+//             Container(
+//               width: 90.w,
+//               height: 90.h,
+//               decoration: BoxDecoration(
+//                 shape: BoxShape.circle,
+//                 image: DecorationImage(
+//                   image: caseModel.photos.isNotEmpty
+//                       ? CachedNetworkImageProvider(caseModel.photos.first.url!)
+//                       : const AssetImage(
+//                           "assets/images/Portrait_Placeholder.png"),
+//                   fit: BoxFit.cover,
+//                 ),
+//               ),
+//             ),
+//             SizedBox(width: 16.w),
+//             Expanded(
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Row(
+//                     crossAxisAlignment: CrossAxisAlignment.center,
+//                     children: [
+//                       Expanded(
+//                         child: Column(
+//                           crossAxisAlignment: CrossAxisAlignment.start,
+//                           children: [
+//                             Row(
+//                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                               children: [
+//                                 Flexible(
+//                                   child: Text(
+//                                     "${caseModel.firstName} ${caseModel.lastName}",
+//                                     style: TextStyle(
+//                                       fontSize: 16.sp,
+//                                       fontWeight: FontWeight.bold,
+//                                       color: Colors.black,
+//                                     ),
+//                                     softWrap: true,
+//                                   ),
+//                                 ),
+//                                 if (caseModel.userId.toString() ==
+//                                     sl<AuthLocal>().getUserId())
+//                                   IconButton(
+//                                     onPressed: () {
+//                                       showModalBottomSheet(
+//                                         context: context,
+//                                         builder: (_) => Column(
+//                                           mainAxisSize: MainAxisSize.min,
+//                                           children: [
+//                                             ListTile(
+//                                               leading: const Icon(Icons.edit),
+//                                               title: const Text('Edit'),
+//                                               onTap: () {
+//                                                 Navigator.pop(context);
+//                                                 // كود الـ edit
+//                                                 context.toNamed(
+//                                                     AppRoutes.addCaseView,
+//                                                     arguments: {
+//                                                       'case': caseModel,
+//                                                       'cubit': context.read<
+//                                                           AllCasesCubit>(),
+//                                                     });
+//                                               },
+//                                             ),
+//                                             ListTile(
+//                                               leading: const Icon(Icons.delete,
+//                                                   color: Colors.red),
+//                                               title: const Text('Delete',
+//                                                   style: TextStyle(
+//                                                       color: Colors.red)),
+//                                               onTap: () async {
+//                                                 Navigator.pop(context);
+
+//                                                 await context
+//                                                     .read<AllCasesCubit>()
+//                                                     .deleteCaseOptimistic(
+//                                                       context: context,
+//                                                       caseModel: caseModel,
+//                                                     );
+//                                               },
+//                                             ),
+//                                           ],
+//                                         ),
+//                                       );
+//                                     },
+//                                     icon: const Icon(Icons.more_vert),
+//                                   ),
+//                               ],
+//                             ),
+//                             SizedBox(height: 4.h),
+//                             Row(
+//                               children: [
+//                                 Icon(Icons.location_on,
+//                                     size: 14.w, color: Colors.red),
+//                                 SizedBox(width: 4.w),
+//                                 Text(
+//                                   caseModel.address ?? '',
+//                                   style: TextStyle(
+//                                       fontSize: 12.sp, color: Colors.grey[600]),
+//                                 ),
+//                               ],
+//                             ),
+//                           ],
+//                         ),
+//                       ),
+//                       if (caseModel.similarityScore != null)
+//                         BalanceIndicator(
+//                           similarityScore: caseModel.similarityScore!,
+//                           desText: null,
+//                           radius: 30,
+//                           available: "",
+//                         ),
+//                     ],
+//                   ),
+//                   SizedBox(height: 12.h),
+
+//                   // Action Buttons
+//                   Row(
+//                     children: [
+//                       // Info Button
+//                       GestureDetector(
+//                         onTap: () {
+//                           context.toNamed(
+//                             AppRoutes.caseInfoRoute,
+//                             arguments: {
+//                               'case': caseModel,
+//                               'cubit': context.read<AllCasesCubit>(),
+//                             },
+//                           );
+//                         },
+//                         child: Container(
+//                           padding: EdgeInsets.symmetric(
+//                               horizontal: 16.w, vertical: 6.h),
+//                           decoration: BoxDecoration(
+//                             color: AppColors.mainColor,
+//                             borderRadius: BorderRadius.circular(16.r),
+//                           ),
+//                           child: Text(
+//                             "Info",
+//                             style: TextStyle(
+//                               fontSize: 12.sp,
+//                               color: Colors.white,
+//                               fontWeight: FontWeight.w600,
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                       SizedBox(width: 12.w),
+
+//                       // Action Icons
+//                       ActionIcon(
+//                         icon: SvgPicture.asset(
+//                           'assets/icons/comment_icon.svg',
+//                           width: 16,
+//                           height: 16,
+//                         ),
+//                       ),
+//                       SizedBox(width: 2.w),
+
+//                       const ActionIcon(
+//                           icon: const Icon(
+//                         Icons.info_outline,
+//                         color: AppColors.mainColor,
+//                         size: 16,
+//                       )),
+//                       SizedBox(width: 2.w),
+
+//                       const ActionIcon(
+//                           icon: const Icon(
+//                         Icons.help_outline,
+//                         color: AppColors.mainColor,
+//                         size: 16,
+//                       )),
+//                       SizedBox(width: 2.w),
+
+//                       BlocBuilder<AllCasesCubit, AllCasesState>(
+//                         builder: (context, state) {
+//                           final updatedCase = state.filtered.firstWhere(
+//                             (c) => c.id == caseModel.id,
+//                             orElse: () => caseModel,
+//                           );
+
+//                           return ActionIcon(
+//                             icon: Icon(
+//                               updatedCase.isLiked
+//                                   ? Icons.favorite
+//                                   : Icons.favorite_border,
+//                               color: AppColors.mainColor,
+//                               size: 16,
+//                             ),
+//                             onTap: () async {
+//                               // Optimistic UI: قلب فورًا
+//                               final previousValue = updatedCase.isLiked;
+//                               context.read<AllCasesCubit>().updateCaseLike(
+//                                     updatedCase.id ?? -1,
+//                                     !previousValue,
+//                                   );
+
+//                               try {
+//                                 // Cubit يتعامل مع request ويرجع isLiked
+//                                 await context.read<AllCasesCubit>().toggleLike(
+//                                       updatedCase.id ?? -1,
+//                                     );
+//                               } catch (e) {
+//                                 print('Like toggle failed: $e');
+
+//                                 // ارجع القيمة القديمة لو فشل
+//                                 context.read<AllCasesCubit>().updateCaseLike(
+//                                     updatedCase.id ?? -1, previousValue);
+//                               }
+//                             },
+//                           );
+//                         },
+//                       ),
+//                     ],
+//                   ),
+//                   SizedBox(height: 16.w),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -84,19 +323,12 @@ class CaseCard extends StatelessWidget {
                   ),
                 ),
 
-                /// الاسم فوق الصورة
-                Positioned(
-                  left: 12.w,
-                  bottom: 12.h,
-                  child: Text(
-                    "${caseModel.firstName} ${caseModel.lastName}",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
+                // /// الاسم فوق الصورة
+                // Positioned(
+                //   left: 12.w,
+                //   bottom: 12.h,
+
+                // ),
 
                 /// زر options
                 if (caseModel.userId.toString() == sl<AuthLocal>().getUserId())
@@ -159,6 +391,15 @@ class CaseCard extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  Text(
+                    "${caseModel.firstName} ${caseModel.lastName}",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
                   /// location
                   Row(
                     children: [
