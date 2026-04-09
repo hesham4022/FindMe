93db9e68a2d7a543e815795c2e6e4dec85ba8bb8
Hesham Hamd <heshamrdwan2004@gmail.com>
2026-04-09 20:37:54 +0200

    auto-commit

--- lib/features/notifications/data/source/pusher.dart
+++ lib/features/notifications/data/source/pusher.dart
@@ -11,45 +11,34 @@ class PusherService {
     await _pusher.init(
       apiKey: "a118360ad1a87f1ee1ae",
       cluster: "mt1",
-
       authEndpoint:
           "https://web-production-2673c.up.railway.app/api/broadcasting/auth",
-
-      // ✅ أهم تعديل هنا
       authParams: {
         'headers': {
           'Authorization': 'Bearer $token',
           'Accept': 'application/json',
         }
       },
-
       onConnectionStateChange: (currentState, previousState) {
-        print("🟢 STATE: $currentState");
+        print("STATE: $currentState");
       },
-
       onSubscriptionSucceeded: (channelName, data) {
-        print("✅ SUBSCRIBED: $channelName");
+        print("SUBSCRIBED: $channelName");
       },
-
       onSubscriptionError: (message, e) {
-        print("❌ SUB ERROR: $message , $e");
+        print("SUB ERROR: $message , $e");
       },
-
       onEvent: (event) {
-        print("🔥 EVENT: ${event.eventName}");
-        print("🔥 CHANNEL: ${event.channelName}");
-        print("🔥 DATA: ${event.data}");
+        print("EVENT: ${event.eventName}");
+        print("DATA: ${event.data}");
       },
-
       onError: (message, code, e) {
-        print("❌ ERROR: $message | $code | $e");
+        print("ERROR: $message | $code | $e");
       },
     );
 
-    // ✅ connect الأول
     await _pusher.connect();
 
-    // ✅ بعدين subscribe
     await _pusher.subscribe(
       channelName: "private-App.Models.User.$userId",
     );
